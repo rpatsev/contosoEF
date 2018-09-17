42d3598f3d9e55139da0da6e06c154618999a82e
Roman Patsev <roman.patsev@nixsolutions.com>
2018-09-17 18:53:22 +0300

    Initial commit

---
+++ ContosoApp/DAL/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using ContosoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ContosoApp.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+        }
+
+    }
+}
